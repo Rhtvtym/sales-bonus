25dd640705a7589e4f91f8126e1d461e1f907997
Yuliya Kim <pmk678@gmail.com>
2025-06-28 20:41:08 +0300

    fix function calculateBonusByProfit 1

--- src/main.js
+++ src/main.js
@@ -19,17 +19,17 @@ function calculateSimpleRevenue(purchase, _product) {
  * @returns {number}
  */
 function calculateBonusByProfit(index, total, seller) {
-    // @TODO: Расчет бонуса от позиции в рейтинге
-    index += 1 // Для удобства (оригинальные индексы начинаются с 0)
-    if (index === 1) {
-        return 0.15 * seller.profit;
-    } else if (index === 2 || index === 3) {
-        return 0.10 * seller.profit;
-    } else if (index === total) {
-        return 0;
-    } else { // Для всех остальных
-        return 0.05 * seller.profit;
-    } 
+    const { profit } = seller;
+
+    if (index === 0) {
+        return bonus = (profit / 100 ) * 15;
+    } else if (index === 1 || index === 2) {
+        return bonus = (profit / 100 ) * 10;
+    } else if (index === total - 1) {
+        return bonus = 0;
+    } else {
+        return bonus = (profit / 100 ) * 5;
+    }
 }
 /**
  * Функция для анализа данных продаж
